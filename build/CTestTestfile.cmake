76551aecf3304f5782af6b2b35078ae849ce52d4
Arthur Glandier <glandier1u@etu.univ-lorraine.fr>
2021-10-05 15:45:21 +0200

    decommente

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: O:/1A/FirstOrganizedCMake
+# Build directory: O:/1A/FirstOrganizedCMake/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src/log_lib")
+subdirs("tests")
