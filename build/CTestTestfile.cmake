5cda18fa26d3308f7775eeddafde9b98be88991d
Arthur Glandier <glandier1u@etu.univ-lorraine.fr>
2021-10-05 14:50:23 +0200

    deleted build

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: O:/1A/FirstOrganizedCMake
-# Build directory: O:/1A/FirstOrganizedCMake/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/log_lib")
-subdirs("mains")
-subdirs("tests")
